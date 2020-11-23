bf9998c59311442f6631481c5bdbfe45663faa2e
Melodie Souksone <msouksone@gmail.com>
2020-11-23 18:24:31 +0100

    model preparation and review without db:migrate

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,8 @@
 class Menu < ApplicationRecord
   belongs_to :user
+  has_many :recipes, through: :menu_recipes
+  has_many :menu_recipes
+  validates :diet, presence: true
+  validates :number_of_people, presence: true
+  validates :number_of_meals, presence: true
 end
