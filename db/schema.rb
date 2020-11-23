6b2f143cb4cbd7bb18c54ff9857d9cb2a8d15676
Benjamin Barbier <benjamin.barbier@gmail.com>
2020-11-23 18:16:16 +0100

    temporary models

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_163344) do
-
+ActiveRecord::Schema.define(version: 2020_11_23_170634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.string "unit"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "menus", force: :cascade do |t|
     t.string "diet"
