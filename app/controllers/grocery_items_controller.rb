3db863c0fa0b06032dec6691f3e30ba264946058
Melodie Souksone <msouksone@gmail.com>
2020-11-23 20:50:15 +0100

    controllers created and has many and belongs to added in models

---
+++ app/controllers/grocery_items_controller.rb
@@ -0,0 +1,2 @@
+class GroceryItemsController < ApplicationController
+end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,8 +1,11 @@
 class Menu < ApplicationRecord
+  DIET = []
   belongs_to :user
   has_many :recipes, through: :menu_recipes
   has_many :menu_recipes
+  has_many :grocery_items
   validates :diet, presence: true
   validates :number_of_people, presence: true
   validates :number_of_meals, presence: true
+  validates_inclusion_of :diet, in :DIET
 end
