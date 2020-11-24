d5468a302bc7693bc456075cf1102d7c5a4570e3
Melodie Souksone <msouksone@gmail.com>
2020-11-24 11:52:45 +0100

    models modification

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,12 +1,11 @@
 class Menu < ApplicationRecord
-  DIET = []
-  has_many :recipes, through: :menu_recipes
   has_many :menu_recipes
+  has_many :recipes, through: :menu_recipes
   has_many :grocery_items
   belongs_to :user
-  
+
   validates :diet, presence: true
   validates :number_of_people, presence: true
   validates :number_of_meals, presence: true
-  validates_inclusion_of :diet, in :DIET
+  validates_inclusion_of :diet, in: Recipe::DIET
 end
