95138246ba9cc9e40e28e3fef951ec10c8b1e553
Melodie Souksone <msouksone@gmail.com>
2020-11-24 11:32:49 +0100

    pundit controller installation

---
+++ app/policies/grocery_item_policy.rb
@@ -0,0 +1,7 @@
+class GroceryItemPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
---
+++ app/policies/preparation_policy.rb
@@ -0,0 +1,7 @@
+class PreparationPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
