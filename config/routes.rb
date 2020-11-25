f5d083d69542e2540a7b0eb591dd24e185764e29
Melodie Souksone <msouksone@gmail.com>
2020-11-25 16:36:11 +0100

    review controller created with routes and ugly view (javascript included)

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :menu_recipes, only: [ :show, :update, :destroy ] do
     resources :reviews, only: [ :create ]
   end
+
 # ajouter 2 spéciales : grocery et menu_recipes patch
   # get 'users/toys', to: "toys#show_user_toy", as: :user_toys
   post '/menus/:id/switch', to: "menu_recipes#switch"
