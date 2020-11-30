a210f2e50aa6d11d4b68748a056405c8f8b09dc8
Benjamin Barbier <benjamin.barbier@gmail.com>
2020-11-30 18:32:09 +0100

    range styling

--- app/javascript/plugins/display_review_form.js
+++ app/javascript/plugins/display_review_form.js
@@ -1,22 +1,25 @@
 const displayReviewForm = () => {
-  const checkbox = document.querySelector(".check-done")
-  const review_form = document.getElementById('review-form')
+  if (document.getElementById('review-form')) {
 
-  checkbox.addEventListener("click", () => {
-    if (checkbox.checked == true) {
-    review_form.style.display = "block";
-    } else {
-    review_form.style.display = "none";
-    };
-  });
+    const checkbox = document.querySelector(".check-done")
+    const review_form = document.getElementById('review-form')
 
-  window.addEventListener("load", () => {
-    if (checkbox.checked == true) {
-    review_form.style.display = "block";
-    } else {
-    review_form.style.display = "none";
-    };
-  });
+    checkbox.addEventListener("click", () => {
+      if (checkbox.checked == true) {
+      review_form.style.display = "block";
+      } else {
+      review_form.style.display = "none";
+      };
+    });
+
+    window.addEventListener("load", () => {
+      if (checkbox.checked == true) {
+      review_form.style.display = "block";
+      } else {
+      review_form.style.display = "none";
+      };
+    });
+  }
 };
 
 
